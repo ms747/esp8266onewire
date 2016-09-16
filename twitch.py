f08a9e06dfacb1e7e63dbb674e470bd9809dbff6
Mayur Shah <mayur.shah.ha@gmail.com>
2016-09-16 21:17:54 +0530

    Twitch API
    Implementation VIA Python

---
+++ twitch.py
@@ -0,0 +1,40 @@
+import urllib.request
+from pprint import pprint
+import json
+import sys
+
+
+#user = input("Twitch user : ")
+user = str(sys.argv[1])
+url = "https://api.twitch.tv/kraken/streams/" + user
+
+
+#print(url)
+
+responce = urllib.request.urlopen(url)
+
+data = responce.read().decode("utf-8")
+
+#pprint(data)
+
+jdata = json.loads(data)
+
+#pprint(jdata)
+
+#pprint(jdata["stream"])
+
+d2 = str(jdata["stream"])
+#print(d2 == "None")
+
+if d2 == "None" :
+    print("User offline")
+else :
+    print("\n-----------------------User streaming-------------------------\n")
+    print("Name : " + str(jdata["stream"]["channel"]["display_name"]))    
+    print("Game : " + str(jdata["stream"]["channel"]["game"]))    
+    print("Language : " + str(jdata["stream"]["channel"]["language"]))    
+    print("Viewers : " + str(jdata["stream"]["viewers"]))    
+    print("--------------------------------------------------------------\n")
+    print("\n\n")
+
+
