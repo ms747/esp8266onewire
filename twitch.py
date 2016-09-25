9fdf76e11e5be67e9a01ce7ae00732b7c69e019b
Mayur Shah <mayur.shah.ha@gmail.com>
2016-09-25 12:55:02 +0530

    Twitch api now requires client id
    Client id added

--- twitch.py
+++ twitch.py
@@ -6,7 +6,8 @@ import sys
 
 #user = input("Twitch user : ")
 user = str(sys.argv[1])
-url = "https://api.twitch.tv/kraken/streams/" + user
+client = "4u0hc5wbl14ppu8620x75j95mc2chjs"
+url = "https://api.twitch.tv/kraken/streams/" + user + "?client_id=" + client
 
 
 #print(url)
